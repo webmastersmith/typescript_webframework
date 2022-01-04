2056c4ba97713767a525e8ff50772bac3c83308e
Bryon Smith <bryon@smithauto.us>
2022-01-03 18:17:01 -0600

    add models with Attributes Events Sync

---
+++ src/models/Sync.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosPromise } from 'axios'
+import { UserProps } from './User'
+
+interface HasId {
+  id?: number
+}
+export class Sync<T extends HasId> {
+  // url: string = 'http://localhost:3000/users'
+
+  constructor(public url: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.url}/${id}`)
+  }
+
+  save(data: T): AxiosPromise {
+    const id = data?.id
+    // if user id exist, just update user
+    return id
+      ? axios.put(`${this.url}/${id}`, data)
+      : axios.post(this.url, data)
+  }
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,15 +1,21 @@
-interface UserProps {
-  name: string
-  age: number
+import { Eventing } from './Eventing'
+import { Sync } from './Sync'
+import { Attributes } from './Attributes'
+
+export interface UserProps {
+  name?: string
+  age?: number
+  id?: number
 }
 
 export class User {
-  constructor(private data: UserProps) {}
+  public events: Eventing = new Eventing()
+  public sync: Sync<UserProps> = new Sync<UserProps>(
+    'http://localhost:3000/users'
+  )
+  public attributes: Attributes<UserProps>
 
-  get(propName: string): string | number {
-    return this.data[propName]
-  }
-  set(propName: string, value: string | number): void {
-    this.data[propName] = value
+  constructor(public attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs)
   }
 }
